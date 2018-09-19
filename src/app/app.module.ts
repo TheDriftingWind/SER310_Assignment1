d1175bc427a34cc6812fd5fef53b75ed30baa0e2
Charles Zhu <CharlesYZhu@yahoo.com>
2018-09-19 17:35:58 -0400

    Assignment 2
    Additions for assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,29 @@ import { AboutComponent } from './about/about.component';
 import { ShowComponent } from './show/show.component';
 import { TestComponent } from './test/test.component';
 
+import { Routes, RouterModule } from '@angular/router';
+import { StaffComponent } from './staff/staff.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'staff', component: StaffComponent }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     AboutComponent,
     ShowComponent,
-    TestComponent
+    TestComponent,
+    StaffComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
