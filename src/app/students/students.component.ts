9230c54443b9b95aad97f4da61afe9b3bcf25c7c
Charles Zhu <CharlesYZhu@yahoo.com>
2018-09-26 18:05:04 -0400

    Practice 3
    Student page for practice 3

--- src/app/students/students.component.ts
+++ src/app/students/students.component.ts
@@ -9,7 +9,7 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
 export class StudentsComponent {
-  images = ["../assets/images/students/deku_punch_robot.jpg", "../assets/images/students/bakugo_fighting.jpg", "../assets/images/students/todoroki_rescue.png", "../assets/images/students/Recipro_Kick.gif"];
+  images = ["../assets/images/students/deku_fights_muscular.gif", "../assets/images/students/bakugo_escape.gif", "../assets/images/students/todoroki_fights_stain.gif", "../assets/images/students/iida_kick.gif"];
 
   constructor(config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
