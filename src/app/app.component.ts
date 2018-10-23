99a0e88fa2aa620613de52e0cdf2636bdc3a1e47
Charles Zhu <CharlesYZhu@yahoo.com>
2018-10-23 17:31:24 -0400

    nav bar feedback

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'U.A. Academy';
   public isCollapsed  = true;
+  currentUrl : string = '';
+
+  constructor(private router: Router){
+    this.router.events.subscribe((event:Event) => {
+		if(event instanceof NavigationEnd ){
+
+			this.currentUrl = event.url;
+
+			console.log(this.currentUrl);
+			console.log(this.currentUrl == '/');
+		}
+	});
+  }
+
 }
